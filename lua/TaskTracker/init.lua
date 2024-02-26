f2bbbec3d659304061906e12565739c6a1961c49
lucashdez <lukastpiano@gmail.com>
2024-02-26 17:42:54 +0000

    ??

--- lua/TaskTracker/init.lua
+++ lua/TaskTracker/init.lua
@@ -15,9 +15,24 @@ end
 function M.start_timer()
 	vim.ui.input({
 		prompt = "Task name: ",
+		completion = "customList",
 	}, function(args)
 		-- vim.api.nvim_open_win(buffer: integer, enter: boolean, config?: table<string, any>)
 		-- -complete=customlist,{func} custom completion, defined via {func}
+		--[[
+		function TaskTrackerCompletion(args, cmd, cpos)
+			local tt = require("TaskTracker")
+			local splitCmd = vim.split(cmd, " ")
+			local completions = {}
+			for k, t in pairs(tt) do
+				if type(t) == "function" then
+					table.insert(completions, tostring(k))
+				end
+			end
+			return vim.tbl_filter(function(key)
+				return key:find(splitCmd[#splitCmd])
+			end, completions)
+		end]]
 		M.name = args
 		M.stime = os.time()
 		M.tracking = true
